cfbc0330022c1544c7c4ae8b0488b872b82623f3
schiptsova.n <schiptsova.n@dmze.milandr.ru>
2022-09-27 21:48:14 +0300

    add translate and banner

--- spring-04/src/main/java/ru/otus/spring/dao/CsvQuestionDao.java
+++ spring-04/src/main/java/ru/otus/spring/dao/CsvQuestionDao.java
@@ -1,26 +1,37 @@
 package ru.otus.spring.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import ru.otus.spring.SpringBootApplicationMain;
+import ru.otus.spring.config.ApplicationConfig;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @Component
 public class CsvQuestionDao implements QuestionDao {
-    @Value("${questions.resource.file}")
+    @Value("${questions.resource.file.en}")
     private String questionResource;
 
-    public CsvQuestionDao() {
+    @Value("${questions.resource.file.ru}")
+    private String questionResourceRu;
+
+    private final ApplicationConfig applicationConfig;
+
+    @Autowired
+    public CsvQuestionDao(ApplicationConfig applicationConfig) {
+        this.applicationConfig = applicationConfig;
     }
 
     @Override
     public List<String> getQuestions() {
-        InputStream in = SpringBootApplicationMain.class.getClassLoader().getResourceAsStream(this.questionResource);
+        InputStream in = SpringBootApplicationMain.class.getClassLoader().getResourceAsStream(!applicationConfig.getLocale().equals(Locale.ENGLISH) ?
+                this.questionResourceRu : this.questionResource);
         return new BufferedReader(new InputStreamReader(in))
                 .lines()
                 .collect(Collectors.toList());
