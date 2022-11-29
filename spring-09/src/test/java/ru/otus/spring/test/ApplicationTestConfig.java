cb2523b5e9b91873be1f25855873c778230f0a89
natalia_shchiptsova <natalia_shchiptsova@belkatechnologies.com>
2022-11-30 00:22:24 +0200

    added comment entity

--- spring-09/src/test/java/ru/otus/spring/test/ApplicationTestConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.test;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.TestPropertySource;
-
-@SpringBootApplication(scanBasePackages = {"ru.otus.spring.service", "ru.otus.spring.dao"})
-public class ApplicationTestConfig {
-
-}
